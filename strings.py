10cae23b99e3b8c3b8589b5cab35a923b5cd9645
Prakash-Sarkar <prakashsarkar43@gmail.com>
2024-08-11 22:08:23 -0600

    practice

--- strings.py
+++ strings.py
@@ -1,1 +1,17 @@
-print ()
+#message = "Bobby's World"
+#print (message)
+
+# message = 'Bobby\'s World'
+# print (message)
+
+#message = """Bobb's World"""
+#print (message)
+
+#to check length:
+# message = "Hello World"
+# print (len(message))
+
+#to check any letter:
+# message = "Hello World"
+# print (message[1])
+
