43cdcb19e9d2fcdf0c3f9e48b381b5932716dfd5
Prakash-Sarkar <prakashsarkar43@gmail.com>
2024-08-13 18:31:36 -0600

    string update

--- strings.py
+++ strings.py
@@ -15,3 +15,43 @@
 # message = "Hello World"
 # print (message[1])
 
+#only to print out specific message:
+# message = "Hello World"
+# print (message[0:5])
+
+#now we are gonna print out specific message by slicing:
+# message = "Hello World"
+# print (message[6:])
+
+#to make lower case all the words:
+# message = "Hello World"
+# print (message.lower())
+
+#to make upper case all the words:
+# message = "Hello World"
+# print (message.upper())
+
+#to find any words:
+# message = "Hello World"
+# print (message.find('World'))#if it not finding anything then it will provide negetive value
+
+#to replace anywords:
+# message = "Hello World"
+#
+# message=message.replace('World','Universe')
+# print (message)
+
+#to make a combined lines:
+# greeting ='Hello'
+# name = 'Prakash'
+#
+# message = '{}, {}. Welcome!'.format(greeting, name)
+# print(message)
+
+#with f string to make combined lines are easy:
+# greeting = 'Hello'
+# name = 'Prakash'
+#
+# message = f'{greeting}, {name}. Welcome!'
+# print(message)
+
