8d492ebd656281f843ebeea09baa0d7d4bbeb19f
Prakash-Sarkar <prakashsarkar43@gmail.com>
2024-08-13 19:22:10 -0600

    numeric data update

---
+++ Numeric Data.py
@@ -0,0 +1,20 @@
+# print(round(3.75))
+# print(round(3.49))
+
+#to make 1 digit after decimal:
+# print(round(3.75,1))
+
+#comparison:
+# num_1 = 2
+# num_2 = 3
+#
+# print(num_1 == num_2)
+
+#numbers but works like case:
+# num_1 = '100'
+# num_2 = '200'
+#
+# num_1 = int(num_1)
+# num_2 = int(num_2)
+#
+# print(num_1 + num_2)
