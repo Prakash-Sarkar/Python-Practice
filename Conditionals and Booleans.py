5e12293d2cc337607faf3162e999984782711bae
Prakash-Sarkar <prakashsarkar43@gmail.com>
2024-08-21 22:59:08 -0600

    Conditional and Booleans

---
+++ Conditionals and Booleans.py
@@ -0,0 +1,26 @@
+# language = 'java'
+#
+# if language == 'python':
+#     print('language is python')
+# elif language == 'java':
+#     print('language is java')
+# elif language == 'javascript':
+#     print('language is javascript')
+# else:
+#     print('no match')
+
+
+# user = 'admin'
+# logged_in = False
+# if user == 'admin' or logged_in:
+#     print('admin page')
+# else:
+#     print('bad creds')
+
+#condition = 1
+condition = ('test')
+
+if condition:
+    print('evaluated to true')
+else:
+    print('evaluated to false')
