5fcf156eb7ac9574d641e495c6bbea9b3ad1128c
Prakash-Sarkar <prakashsarkar43@gmail.com>
2024-08-23 23:20:08 -0600

    loops and iterations

---
+++ Loops and Iterations.py
@@ -0,0 +1,35 @@
+#For Loops:
+# nums = [1, 2, 3, 4, 5]
+#
+# for num in nums:
+#     if num == 3:
+#         print('found!')
+#         break
+#     print(num)
+
+#to make it continue:
+nums = [1, 2, 3, 4, 5]
+
+for num in nums:
+    if num == 3:
+        print('found!')
+        continue
+    print(num)
+
+#while loops:
+# x = 0
+#
+# while True:
+#     if x == 5:
+#         break
+#     print (x)
+#     x +=1
+
+#infinite loops:
+# x = 0
+#
+# while True:
+#     # if x == 5:
+#     #     break
+#     print (x)
+#     x +=1
