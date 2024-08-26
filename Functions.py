b4c35e1438a066c62adbd56f8307b850b0c24347
Prakash-Sarkar <prakashsarkar43@gmail.com>
2024-08-25 21:07:17 -0600

    functions update

--- Functions.py
+++ Functions.py
@@ -1,7 +1,16 @@
-def hello_func():
-    print('Hello Function.')
+# def hello_func():
+#     print('Hello Function.')
+#
+# hello_func()
+# hello_func()
+# hello_func()
+# hello_func()
 
-hello_func()
-hello_func()
-hello_func()
-hello_func()
+def student_info(*arg, **kwargs):
+    print(arg)
+    print(kwargs)
+
+courses = ['math', 'art']
+info = {'name': 'john', 'age': '22'}
+
+student_info(*courses, **info)
